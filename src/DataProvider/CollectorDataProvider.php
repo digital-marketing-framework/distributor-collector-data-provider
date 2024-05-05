9d9a084c43f0823fca2ac400ade288e7266d7b29
s-ude <ude@mediatis.de>
2024-05-05 22:38:22 +0200

    [TASK] Rework Configuration Structure (MFR-74) (#2)

--- src/DataProvider/CollectorDataProvider.php
+++ src/DataProvider/CollectorDataProvider.php
@@ -4,13 +4,13 @@ namespace DigitalMarketingFramework\Distributor\CollectorDataProvider\DataProvid
 
 use DigitalMarketingFramework\Collector\Core\Model\Configuration\CollectorConfiguration;
 use DigitalMarketingFramework\Collector\Core\Service\CollectorInterface;
-use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\ContainerSchema;
-use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\CustomSchema;
-use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\Plugin\DataProcessor\DataMapperSchema;
-use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\SchemaInterface;
 use DigitalMarketingFramework\Core\Context\ContextInterface;
 use DigitalMarketingFramework\Core\DataProcessor\DataProcessorAwareInterface;
 use DigitalMarketingFramework\Core\DataProcessor\DataProcessorAwareTrait;
+use DigitalMarketingFramework\Core\SchemaDocument\Schema\ContainerSchema;
+use DigitalMarketingFramework\Core\SchemaDocument\Schema\Custom\DataMapperGroupReferenceSchema;
+use DigitalMarketingFramework\Core\SchemaDocument\Schema\CustomSchema;
+use DigitalMarketingFramework\Core\SchemaDocument\Schema\SchemaInterface;
 use DigitalMarketingFramework\Distributor\Core\DataProvider\DataProvider;
 use DigitalMarketingFramework\Distributor\Core\Model\DataSet\SubmissionDataSetInterface;
 use DigitalMarketingFramework\Distributor\Core\Registry\RegistryInterface;
@@ -33,14 +33,18 @@ class CollectorDataProvider extends DataProvider implements DataProcessorAwareIn
     protected function processContext(ContextInterface $context): void
     {
         $configuration = CollectorConfiguration::convert($this->submission->getConfiguration());
-        $this->collector->prepareContext($configuration, $this->submission->getContext());
+        $this->collector->prepareContext($configuration, context: $this->submission->getContext());
     }
 
     protected function process(): void
     {
         $configuration = CollectorConfiguration::convert($this->submission->getConfiguration());
-        $data = $this->collector->collect($configuration, $this->submission->getContext());
-        $data = $this->dataProcessor->processDataMapper($this->getConfig(static::KEY_DATA_MAP), $data, $this->submission->getConfiguration());
+        $data = $this->collector->collect($configuration, preparedContext: $this->submission->getContext());
+
+        $dataMapperGroupId = $this->getConfig(static::KEY_DATA_MAP);
+        $dataMapperGroupConfig = $this->submission->getConfiguration()->getDataMapperGroupConfiguration($dataMapperGroupId);
+        $context = $this->dataProcessor->createContext($data, $this->submission->getConfiguration());
+        $data = $this->dataProcessor->processDataMapperGroup($dataMapperGroupConfig, $context);
         foreach ($data as $field => $value) {
             $this->setField($field, $value);
         }
@@ -50,7 +54,7 @@ class CollectorDataProvider extends DataProvider implements DataProcessorAwareIn
     {
         /** @var ContainerSchema $schema */
         $schema = parent::getSchema();
-        $schema->addProperty(static::KEY_DATA_MAP, new CustomSchema(DataMapperSchema::TYPE));
+        $schema->addProperty(static::KEY_DATA_MAP, new CustomSchema(DataMapperGroupReferenceSchema::TYPE));
 
         return $schema;
     }
